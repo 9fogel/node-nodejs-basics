69938df7ebdf6d6c2134e003496cf2776ce44b94
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-15 17:30:43 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(filePath, { encoding: 'utf8' });
+    console.log(content);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
