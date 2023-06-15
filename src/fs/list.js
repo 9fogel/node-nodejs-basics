b648844011a3ef660acdb24777bc3947d46e1000
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-15 17:26:58 +0300

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filesFolderPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const filesList = await readdir(filesFolderPath);
+    console.log(filesList);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
