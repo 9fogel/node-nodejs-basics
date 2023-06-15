789612f2f305c404a28b72211efc743de1b5060d
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-15 16:15:46 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import { mkdir, readdir, copyFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const newDirPath = path.join(__dirname, 'files_copy');
+const filesFolderPath = path.join(__dirname, 'files');
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await mkdir(newDirPath);
+    const files = await readdir(filesFolderPath);
+    for (let file of files) {
+      await copyFile(path.join(filesFolderPath, file), path.join(newDirPath, file));
+    }
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
