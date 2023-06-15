93161fb9b7b42b077c078da49ef845794a43b138
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-15 16:54:58 +0300

    feat: add partial solution for rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import { access, rename as renameFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+const newPath = path.join(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    //TODO: properFilename.md already exists - error must be thrown
+    await renameFile(oldPath, newPath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
