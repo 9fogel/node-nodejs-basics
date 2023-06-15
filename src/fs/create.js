5d42d56f9293b6c27a0c4e655d40fb995ba2bf45
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-15 15:48:33 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+const text = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(filePath, text, { flag: 'wx' });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
