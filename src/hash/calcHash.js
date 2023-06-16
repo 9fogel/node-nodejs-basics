a14e34b87a65145d1c0ace2f1310624d25f59e55
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-16 17:06:24 +0300

    feat: add calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+const { createHash } = await import('crypto');
+
+const hash = createHash('sha256');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = createReadStream(filePath);
+  readStream.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
