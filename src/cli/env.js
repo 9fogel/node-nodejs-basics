c53708be306dac80785872cad8d1c4fe70783b20
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-16 14:47:16 +0300

    refactor: add indents for parseEnv

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,8 +1,8 @@
 const parseEnv = () => {
   const parsedArgs = Object.entries(process.env)
-  .filter((item) => item[0].startsWith('RSS'))
-  .map((item) => `${item[0]}=${item[1]}`)
-  .join('; ');
+    .filter((item) => item[0].startsWith('RSS'))
+    .map((item) => `${item[0]}=${item[1]}`)
+    .join('; ');
 
   console.log(parsedArgs);
 };
