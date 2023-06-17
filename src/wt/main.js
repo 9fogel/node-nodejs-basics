71967bf846728fff46b53c156fef119fd5d221a0
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-18 00:24:39 +0300

    feat: add wt main.js final solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,25 +1,33 @@
 import os from 'os';
 import path from 'path';
-import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+import { Worker } from 'worker_threads';
 import { getDirname } from '../utils/dirnameHelper.js';
 
 const cpuCoresNumber = os.cpus().length;
 const workerFilePath = path.join(getDirname(import.meta.url), 'worker.js');
 
-const performCalculations = async () => {
-  // if (isMainThread) {
-    for (let i = 0; i < cpuCoresNumber; i++) {
-      const worker = new Worker(workerFilePath, { workerData: i + 10 });
-      worker.on('message', msg => console.log(`Worker message received: ${msg}`));
-      worker.on('error', err => console.error(err));
-      worker.on('exit', (code) => {
-        if (code !== 0)
-          reject(new Error(`Worker stopped with exit code ${code}`));
-      });
-    }
-  // } else {
+const createWorker = (i) => {
+  return new Promise(function (resolve, reject) {
+    const worker = new Worker(workerFilePath, { workerData: i + 10 });
+    worker.on("message", (value) => {
+      resolve({ status: 'resolved', data: value });
+    });
+    worker.on("error", () => {
+      reject({ status: 'error', data: null });
+    });
+  })
+}
 
-  // }
+const performCalculations = async () => {
+  const workerPromises = [];
+
+  for (let i = 0; i < cpuCoresNumber; i++) {
+    workerPromises.push(createWorker(i));
+    }
+
+  const resultsList = await Promise.all(workerPromises);
+
+  console.log(resultsList);
 };
 
 await performCalculations();
