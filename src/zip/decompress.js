fc1017d9bef9d1bcce1522106c4ad0c16b401870
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-17 23:11:53 +0300

    feat: add decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import { createUnzip } from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { getDirname } from '../utils/dirnameHelper.js';
+
+const sourcePath = path.join(getDirname(import.meta.url), 'files', 'archive.gz');
+const destinationPath = path.join(getDirname(import.meta.url), 'files', 'fileToCompress.txt');
+
+const pipe = promisify(pipeline);
+
+const unzip = createUnzip();
+const source = fs.createReadStream(sourcePath);
+const destination = fs.createWriteStream(destinationPath);
+
 const decompress = async () => {
-    // Write your code here 
+  await pipe(source, unzip, destination);
 };
 
 await decompress();
