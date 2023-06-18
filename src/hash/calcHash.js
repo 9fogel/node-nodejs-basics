161b08a606938ae48ad3cbc0a15eab7fff961f53
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-06-18 14:09:24 +0300

    refactor: refactor hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,14 +1,12 @@
+import path from 'path';
+import { getDirname } from '../utils/dirnameHelper.js';
 import { createReadStream } from 'fs';
 import { stdout } from 'process';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
 const { createHash } = await import('crypto');
 
 const hash = createHash('sha256');
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+const filePath = path.join(getDirname(import.meta.url), 'files', 'fileToCalculateHashFor.txt');
 
 const calculateHash = async () => {
   const readStream = createReadStream(filePath);
